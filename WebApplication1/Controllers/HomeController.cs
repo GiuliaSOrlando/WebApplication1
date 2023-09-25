40a74e03daed6fd3846ce720e986a3f3c5b565da
GiuliaSOrlando <brun.ilde@hotmail.com>
2023-09-25 11:45:50 +0300

    Aggiungere i file di progetto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Fattura> listaFatture = new List<Fattura>();
+            listaFatture.Add(new Fattura(new DateTime(2019, 02, 19),"Pinco", 300.00m));
+            listaFatture.Add(new Fattura(new DateTime(2020, 05, 12), "Sempronio", 250.00m));
+            listaFatture.Add(new Fattura(new DateTime(2022, 07, 23), "Caio", 1300.00m));
+            return View(listaFatture);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Fattura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Fattura
+    {
+        public DateTime Datafattura { get; set; }
+        public string Nomecliente { get; set; }
+        public decimal Importo { get; set; }
+
+        public Fattura() { }
+        public Fattura(DateTime datafattura, string nomecliente, decimal importo) {
+        
+            Datafattura = datafattura;
+            Nomecliente = nomecliente;
+            Importo = importo;
+        }
+    }
+}
